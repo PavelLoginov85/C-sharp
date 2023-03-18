d6bbcbe9cebe4688abc51c89eec84de1bc6593a4
Pavel <pahnologia@yandex.ru>
2023-03-18 18:18:59 +0300

    2 задача

--- Program.cs
+++ Program.cs
@@ -15,3 +15,28 @@
 //     else {
 //     Console.WriteLine($"{number} обычное число");
 // }
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+
+// Console.Write("Введите положительное число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// int start = 1;
+// if(num <= 0){
+// Console.Write("Число должно быть больше нуля");
+// }
+// else{
+// while (start <= num)
+// {
+//     double s = Math.Pow(start, 3);
+
+//     Console.Write($"{s }, ");
+//     start++;
+// }
+// }
